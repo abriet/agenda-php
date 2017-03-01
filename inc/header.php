1d2b6ebc697eea0bf6f9ea6850c10a616a5d9b81
Alexandre Briet <alexandrerusilas@gmail.com>
2017-03-01 17:16:48 -0300

    Configurando URL amigavel

--- inc/header.php
+++ inc/header.php
@@ -8,20 +8,20 @@
 <body>
 	<div class="container">
 		<header id="header" class="header">
-			<a href="index.php" title="HOME"><div class="logo">
+			<a href="Home" title="HOME"><div class="logo">
 				<img src="http://2.bp.blogspot.com/-BLs5MduSvJA/TxYl5Pl8mkI/AAAAAAAAAPk/5v6B2hW7Jrc/s400/agenda.png">
 				<h1>Agenda 2017</h1>
 			</div></a>
 			<nav>
 				<ul class="menu">
-					<li><a href="?p=1&pagina=agendar" title="Agendar um horário"><i class="material-icons">add</i>Agendar Horário</a></li>
-					<li><a href="?p=1&pagina=listar" title="Listar Horários"><i class="material-icons">list</i> Listar Horários</a></li>
-					<li><a href="?p=1&pagina=configuracao" title="Configurações da Agenda"><i class="material-icons">settings</i> Configurações</a></li>
+					<li><a href="Agendamento" title="Agendar um horário"><i class="material-icons">add</i>Agendar Horário</a></li>
+					<li><a href="Listagem" title="Listar Horários"><i class="material-icons">list</i> Listar Horários</a></li>
+					<li><a href="Config" title="Configurações da Agenda"><i class="material-icons">settings</i> Configurações</a></li>
 				</ul>
 				<ul class="menu-mobile">
-					<li><a href="?p=1&pagina=agendar" title="Agendar um horário"><i class="material-icons">add</i></a></li>
-					<li><a href="?p=1&pagina=listar" title="Listar Horários"><i class="material-icons">list</i></a></li>
-					<li><a href="?p=1&pagina=configuracao" title="Configurações da Agenda"><i class="material-icons">settings</i></a></li>
+					<li><a href="Agendamento" title="Agendar um horário"><i class="material-icons">add</i></a></li>
+					<li><a href="Listagem" title="Listar Horários"><i class="material-icons">list</i></a></li>
+					<li><a href="Config" title="Configurações da Agenda"><i class="material-icons">settings</i></a></li>
 				</ul>
 			</nav>
 		</header><!-- /header -->
